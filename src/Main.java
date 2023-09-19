bbcb98c34f142f24fa56b5e4f171e83f90ab25e5
epinatci <epakcin@gmail.com>
2023-09-19 15:18:57 +0300

    completed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,77 @@
 public class Main {
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println(isPalindrome(-1221));
+        System.out.println(isPalindrome(707));
+        System.out.println( isPalindrome(11212));
+
+        System.out.println("***********************************");
+
+        System.out.println(isPerfectNumber(6));
+        System.out.println(isPerfectNumber(28));
+        System.out.println(isPerfectNumber(5));
+        System.out.println(isPerfectNumber(-1));
+
+        System.out.println("***********************************");
+
+        System.out.println(numberToWords(123));
+        System.out.println(numberToWords(1010));
+        System.out.println(numberToWords(-12));
+
     }
+
+    public static boolean isPalindrome(int num){
+        if (num < 0) {
+            num = -num; // Negatif sayıyı pozitif yap
+        }
+        int originalNum = num;
+        int reverseNum = 0;
+
+        while (num > 0) {
+            int digit = num % 10;
+            reverseNum = reverseNum * 10 + digit;
+            num /= 10;
+        }
+
+        return originalNum == reverseNum;
+
+    }
+
+    public static boolean isPerfectNumber(int number) {
+        if (number <= 0) {
+            return false;
+        }
+
+        int sumOfDivisors = 0;
+
+        for (int i = 1; i < number; i++) {
+            if (number % i == 0) {
+                sumOfDivisors += i;
+            }
+        }
+
+        return sumOfDivisors == number;
+    }
+
+    public static String numberToWords(int number) {
+        if (number < 0) {
+            return "Invalid Value";
+        }
+
+        String[] units = {
+                "Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"
+        };
+
+        String numStr = String.valueOf(number);
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < numStr.length(); i++) {
+            int digit = Character.getNumericValue(numStr.charAt(i));
+            result.append(units[digit]).append(" ");
+        }
+
+        return result.toString().trim();
+    }
+
 }
